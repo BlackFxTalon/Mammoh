db3995c263386a12d690a6a12774ef3f98be365a
blackfxtalon <andyv@inbox.ru>
2022-09-28 12:53:34 +0500

    removed alpine from main, changed to cdn

--- src/js/modules/alpine.js
+++ src/js/modules/alpine.js
@@ -1,13 +1,13 @@
 /* alpine.js */
 
-import Alpine from 'alpinejs'
+// import Alpine from 'alpinejs'
 /* alpine-collapse.js */
 
-import collapse from '@alpinejs/collapse'
+// import collapse from '@alpinejs/collapse'
 
 
-Alpine.plugin(collapse)
-window.Alpine = Alpine
-Alpine.start()
+// Alpine.plugin(collapse)
+// window.Alpine = Alpine
+// Alpine.start()
 
 
